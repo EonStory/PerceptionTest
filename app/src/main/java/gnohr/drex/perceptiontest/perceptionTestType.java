c25a04dd83d370c5540943f119d9e3459d249450
kommy <notrealaddress@test.com>
2017-03-20 15:30:50 +0000

    Added lots of cool code

---
+++ app/src/main/java/gnohr/drex/perceptiontest/perceptionTestType.java
@@ -0,0 +1,24 @@
+package gnohr.drex.perceptiontest;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Talx on 19/03/2017.
+ */
+
+public abstract class perceptionTestType<T extends Number> {
+
+    protected ArrayList<perceptionDatum<Integer>> data;
+
+    public perceptionTestType() {
+        data = new ArrayList<perceptionDatum<Integer>>();
+    }
+
+
+    public void addPerceptionDatum(perceptionDatum d) {
+        data.add(d);
+    }
+
+
+
+}
