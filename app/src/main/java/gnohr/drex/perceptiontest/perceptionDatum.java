e3f4afaf2e7b051f7fce540c72ae66a0dd08d0dc
kommy <notrealaddress@test.com>
2017-04-03 14:37:38 +0100

    Removed unnecessary comparator interface from perceptionDatum

--- app/src/main/java/gnohr/drex/perceptiontest/perceptionDatum.java
+++ app/src/main/java/gnohr/drex/perceptiontest/perceptionDatum.java
@@ -1,22 +1,33 @@
 package gnohr.drex.perceptiontest;
 
 import java.util.ArrayList;
+import java.sql.Time;
 
-public class perceptionDatum<T extends Number>{
+public class perceptionDatum<T extends Number> {
     //records the time at which the answer was submitted (or timeout reached)
-    private final long timeCompletedAt;
+    private Time timeCompletedAt;
     //Did the user select the correct answer:
-    private final boolean passed;
+    private boolean passed;
     //Stores the values of the 2 stimuli
     private T value1;
     private T value2;
 
-    public perceptionDatum(long timeCompletedAt, boolean passed, T value1, T value2) {
+    public perceptionDatum(Time timeCompletedAt, boolean passed, T value1, T value2) {
         this.timeCompletedAt = timeCompletedAt;
         this.passed = passed;
         this.value1 = value1;
         this.value2 = value2;
     }
 
+    public boolean isSuccess() {
+        return passed;
+    }
 
+    public T getValue1() {
+        return value1;
+    }
+
+    public T getValue2() {
+        return value2;
+    }
 }
